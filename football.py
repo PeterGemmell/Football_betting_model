ac519b4467d440b62c5c989e87feb27d9fe5a0f9
Peter Gemmell <petergemmell65@hotmail.com>
2020-10-21 15:48:54 +0100

    we have bgan looping over the data file to find the teams we are going to use

--- football.py
+++ football.py
@@ -8,6 +8,7 @@ import csv, math, ast, numpy as np
 # Next we have defined the data we want to use, the 2020/2021 season in csv format.
 # We then create an empty list called team_list. We then open a file called team_list.txt and use the 'w' tag
 # after the name to ensure at all the info is deleted.
+# Next we write the first line in the file, which is the start of a dictionary. This is to hold and update our data for each team.
 
 def poisson(actual, mean):
     return math.pow(mean, actual) * math.exp(-mean) / math.factorial(actual)
@@ -19,3 +20,47 @@ team_list = []
 k = open('team_list.txt', 'w')
 k.write("""{
 """)
+
+# Next we want to iterate over our data file and find all the team names that are going to be used.
+# This is done like above where we open the csv file, skip the first line and then create a for loop.
+# The for loop reads both team names and then checks if the names are in our newly created team_list. If they are not, they get added to the list.
+# After it has checked all the team names, it sorts the new list alphabetically.
+
+# The next for loop iterates over over all the teams we find.
+# we will record all the home goals and away goals they score, as well as how many goals they concede both at home and away.
+# We also track the amount of home and away games.
+
+csvRead = csv.reader(open(csvFile))
+next(csvRead)
+
+for row in csvRead:
+    if row[2] not in team_list:
+        team_list.append(row[2])
+    if row[3] not in team_list:
+        team_list.append(row[3])
+
+team_list.sort()
+
+for team in team_list:
+    k.write("""	'%s': {'home_goals': 0, 'away_goals': 0, 'home_conceded': 0, 'away_conceded': 0, 'home_games': 0, 'away_games': 0, 'alpha_h': 0, 'beta_h': 0, 'alpha_a': 0, 'beta_a': 0},
+""" % (team))
+
+# We then write the end of the file and close it. This is so as to save what we have written if we want to open it again.
+
+k.write("}")
+k.close()
+
+# The next two lines creates our dictionary where we will hold and update our data we read.
+# This is done by using the ast module which will read the team_list.txt file and then create a dictionary called ‘dict’.
+
+s = open('team_list.txt', 'r').read()
+dict = ast.literal_eval(s)
+
+GAMES_PLAYED = 0 # This variable is the total tally of games played.
+WEEKS_WAIT = 4 # This could be changeable. This is refering to how many weeks we want to wait before placing a bet.
+TOTAL_VALUE = 0 # This will be sued to update how our betting is doing.
+
+csvRead = csv.reader(open(csvFile))
+next(csvRead)
+
+for
